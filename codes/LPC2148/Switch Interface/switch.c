d0619c7aef1a696c08390a03ad183ec3f6bb2dcf
shashi745 <68228933+shashi745@users.noreply.github.com>
2020-10-02 12:39:39 +0530

    updates switch

--- codes/LPC2148/Switch Interface/switch.c
+++ codes/LPC2148/Switch Interface/switch.c
@@ -1,17 +1,18 @@
+
 #include<lpc214x.h>
 #include<stdint.h>
 
 
 int main(void)
 {
-	IO0DIR=0x00000005; //sets pin 0.0 and 0.2 as output pins
+	IO0DIR=0x0000000F;
 	
 	while(1)
 	{
-		IO0PIN=IO0PIN | 0x00000001; //sets the pin 0.00 as high
-		if((IO0PIN && 0x00000001 )== 0x00000000)//if pin 0.0 is low then set pin 0.2 as high 
+		IO0PIN=IO0PIN | 0x00000001;
+		if((IO0PIN && 0x00000002 )== 0x00000000)
 		{
-		IO0PIN=IO0PIN | 0x00000004;//setting pin 0.2 as high
+		IO0PIN=IO0PIN | 0x00000004;
 		}
 	
 	}
